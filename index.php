173631ede8984ec5d8e9c15f2ef14685faef932d
JekGitHub <jek@free.fr>
2017-02-13 11:40:24 +0100

    tewst

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>		<!-- GitHub C:\xampp\htdocs\JekProjet2017\index.php -->
 <html>
 <head>
-	<title>Projet Git-Hub 2017</title>
+	<title>Projet Git-Hub 2017 (WebForce3) | Page d'Accueil</title>
 </head>
 <body>
-
+	<div>
+		<p>Ma page d'accueil</p>
+	</div>
 </body>
 </html>
